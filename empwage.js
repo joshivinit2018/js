0a2897ea538674041447efca307b430c9d24bf3e
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-20 13:41:51 +0530

    [vinit]:ability to calculate dealy wage

--- empwage.js
+++ empwage.js
@@ -1,13 +1,26 @@
+    const IS_PART_TIME=1;
+    const IS_FULL_TIME=2;
+    const PART_TIME_HOUR=4;
+    const FULL_TIME_HOUR=8;
+    const PER_HOUR_WAGE=20;
 
-
-    const IS_ABSENT=0;
-    let empCheck=Math.floor(Math.random()*10)%2;
-    if (empCheck == IS_ABSENT){
-        console.log("uc1-emp is absent")
+    function get_Working_Hours(empCheck) {
+        switch (empCheck) {
+            case IS_PART_TIME:
+                return PART_TIME_HOUR;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOUR;
+            default:
+                return;
+        }
+        
     }
-        else{
-        console.log("uc1-emp is present");
-    }
+    
+    let empHour =0;
+    let empCheck = Math.floor(Math.random()*10)%3;
+    empHour = get_Working_Hours(empCheck);
+    let empWage = empHour * PER_HOUR_WAGE;
+    console.log("emp Wage : " +empWage); 
 
 
 
