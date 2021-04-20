05b2695001fb2638e7d028d6f47c4dc52973d137
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-20 13:15:33 +0530

    [vinit]:ability to check employee is present or not

--- empwage.js
+++ empwage.js
@@ -1,8 +1,15 @@
-const IS_ABSENT=0;
-let empCheck=Math.floor(Math.random()*10)%2;
-if (empCheck == IS_ABSENT){
-    console.log("uc1-emp is absent")
-}
-else{
-    console.log("uc1-emp is present");
-}
+
+
+    const IS_ABSENT=0;
+    let empCheck=Math.floor(Math.random()*10)%2;
+    if (empCheck == IS_ABSENT){
+        console.log("uc1-emp is absent")
+    }
+        else{
+        console.log("uc1-emp is present");
+    }
+
+
+
+
+
