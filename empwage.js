5490429113e842a769fcbaab2128638eb46a8492
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-20 17:58:49 +0530

    [vinit]: calculate wage per month

--- empwage.js
+++ empwage.js
@@ -1,28 +1,26 @@
-    const IS_PART_TIME=1;
-    const IS_FULL_TIME=2;
-    const PART_TIME_HOUR=4;
-    const FULL_TIME_HOUR=8;
-    const PER_HOUR_WAGE=20;
-
-    function get_Working_Hours(empCheck) {
-        switch (empCheck) {
-            case IS_PART_TIME:
-                return PART_TIME_HOUR;
-            case IS_FULL_TIME:
-                return FULL_TIME_HOUR;
-            default:
-                return;
-        }
-        
-    }
-    
-    let empHour =0;
-    let empCheck = Math.floor(Math.random()*10)%3;
-    empHour = get_Working_Hours(empCheck);
-    let empWage = empHour * PER_HOUR_WAGE;
-    console.log("emp Wage : " +empWage); 
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 2;
+let empHrs = 0;
 
 
-
-
+function getWorkingHours(empCheck){
+	switch(empCheck){
+   	case IS_PART_TIME:
+      	return PART_TIME_HOURS;
+   	case IS_FULL_TIME:
+      	return FULL_TIME_HOURS;
+   	default:
+      	return 0;
+	}
+}
+for (let day =0; day < NUM_OF_WORKING_DAYS; day++){
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total hours : " +empHrs+ " Emp Wage : " +empWage);
 
