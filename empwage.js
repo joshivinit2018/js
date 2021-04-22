7020b30541977af61cd10204b5ac1fd8ac0dac6a
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-22 06:15:37 +0530

    [vinit]: add Using Array Store daily wages in array

--- empwage.js
+++ empwage.js
@@ -3,10 +3,12 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 10;
-const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+
+let empDailyWageArr = new Array();
 
 function getWorkingHours(empCheck){
 	switch(empCheck){
@@ -18,10 +20,18 @@ function getWorkingHours(empCheck){
       	return 0;
 	}
 }
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+
+function calcDailyWageArr(empHrs) {
+	return empHrs * WAGE_PER_HOUR;
+}
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && 
+	totalWorkingDays<NUM_OF_WORKING_DAYS){
 	totalWorkingDays++;
    let empCheck = Math.floor(Math.random() * 10) % 3;
-   totalEmpHrs = getWorkingHours(empCheck);
+	let empHrs= getWorkingHours(empCheck);
+   totalEmpHrs += empHrs; 
+	empDailyWageArr.push(calcDailyWageArr(totalEmpHrs));
 }
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
 console.log("Total Days : " +totalWorkingDays + " Total hours : " +totalEmpHrs+ " Emp Wage : " +empWage);
